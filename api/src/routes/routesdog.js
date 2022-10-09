6b7bc533b676510c712bcf4e719b2471903f7974
Javier <wemakesoftx@gmail.com>
2022-10-09 10:06:59 -0600

    chore: add route to dogById

--- api/src/routes/routesdog.js
+++ api/src/routes/routesdog.js
@@ -4,7 +4,7 @@ const dog = require('../controllers/dogfn.js');
 const routerdog = Router();
 
 routerdog.get('', dog.dogsByName);
-// routerdog.get('/:id', dog.dogById);
+routerdog.get('/:id', dog.dogById);
 // routerdog.post('/', dog.postDog);
 
 module.exports = routerdog;
