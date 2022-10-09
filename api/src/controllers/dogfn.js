3e51b93f978f48a1f6cab76a65e08c8974dec7e4
Javier <wemakesoftx@gmail.com>
2022-10-09 10:09:11 -0600

    feat: add dogById function

--- api/src/controllers/dogfn.js
+++ api/src/controllers/dogfn.js
@@ -9,7 +9,7 @@ const { KEY } = process.env;
  * Función que sirve los datos traidos desde la API externa y BD, puestos
  * a disposición de allDogs para mostrarlos al front.
  *
- * NOT READY JET
+ * READY
  */
 
 getDogs = async (req, res) => {
@@ -20,7 +20,13 @@ getDogs = async (req, res) => {
         id: item.id,
         name: item.name,
         image: item.image.url,
-        weight: item.weight.metric,
+        weight: item.weight.metric,//.split(' - '),
+        // .map(char => {
+        //   return{
+        //     min:char.slice(),
+        //     max:char.slice(1)
+        //   }
+        // }),
         temperament: item.temperament,
       };
     });
@@ -44,6 +50,7 @@ getDogs = async (req, res) => {
  * PONER A DISPOSICIÓN DEL FRONT LA DATA MEDIANTE DOGSBYNAME.
  * allDogs
  * Función que llama a getDogs, sirviendo los datos indirectamente al front. 
+ * READY
  */
 
 allDogs = async() => {
@@ -56,7 +63,7 @@ allDogs = async() => {
  * REALIZAR BUSQUEDA DE DOGS POR NOMBRE.
  * Poner a disposición del front los datos en la ruta principal, determinar
  * si viene una busqueda por query o solo servir los datos generales directamente.
- * NOT READY JET
+ * READY
  */
 dogsByName = async (req, res) => {
   try {
@@ -79,9 +86,48 @@ dogsByName = async (req, res) => {
     console.log(error);
   };
 };
+//========================================================
 
+/**
+ * TRAER DOG POR ID
+ * dogById
+ * Función que trae desde la api externa y la bd, los datos referentes a una
+ * raza en particular, discriminado por el Id.
+ * NOT READY JET
+ */
+
+dogById = async(req, res) => {
+  const { id } = req.params;
+  const idUPPER = id.toUpperCase();
+  try {
+    const getbd = await Dog.findOne({
+      where:{id:idUPPER},
+      include:{
+        model:Temperament,
+        attributes:['name'],
+        through:{attributes:[]},
+      }
+    });
+    console.log('BD', getbd);
+    if(getbd){
+      return res.status(200).send(getbd);
+    }else{
+      const getapi = (await axios.get('https://api.thedogapi.com/v1/breeds')).data;
+      console.log(getapi);
+      //const { name, image, height, weight, life_span, temperament } = getapi;}
+      console.log('params ',id);
+      const gotdata = getapi.filter(filtered => parseInt(filtered.id) === parseInt(id));
+      console.log(gotdata);
+      res.status(200).send(gotdata);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(404).send(`Id ${id} not found`);
+  }
+}
 
 //#######################################################
 module.exports = {
   dogsByName,
+  dogById,
 };
