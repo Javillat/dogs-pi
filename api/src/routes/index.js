82b922167a698fafaa53b8af28b70d48e152b5e3
Javier <wemakesoftx@gmail.com>
2022-10-11 17:35:01 -0600

    chore: add route use to /temperaments

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,13 +2,15 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const routesdog = require('./routesdog.js')
+const routestemperaments = require('./routestemperament.js');
 
 
 const routerMain = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-routerMain.use('/dogs', routesdog)
+routerMain.use('/dogs', routesdog);
+routerMain.use('/temperaments', routestemperaments);
 
 
 module.exports = routerMain;
