a7aa7cc5ef73cc001e3b0a98a358452b73c7eba3
Javier <wemakesoftx@gmail.com>
2022-10-11 17:29:28 -0600

    refacto: change destructuring chargeTemperaments

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const chargeTemperaments = require('./src/controllers/temperamentsfn.js')
+const { chargeTemperaments } = require('./src/controllers/temperamentsfn.js')
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
