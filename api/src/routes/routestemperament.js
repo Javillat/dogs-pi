257f86ec483a3ebd4ea0628cad87c19d067be2f5
Javier <wemakesoftx@gmail.com>
2022-10-11 16:22:23 -0600

    chore: add route to chargeTemperaments

---
+++ api/src/routes/routestemperament.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const temperament = require('../controllers/temperamentsfn.js');
+
+const routertemperament = Router();
+
+//routertemperament.get('/temperaments', temperament.getTemperaments);
+
+module.exports = routertemperament;
