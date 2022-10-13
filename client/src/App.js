d12c527cf2a86065fd1585127758ac76f46ed32a
Javier <wemakesoftx@gmail.com>
2022-10-13 16:12:14 -0600

    feat: add path to /detail/:id

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from '../src/components/home/Home';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "../src/components/home/Home";
+import Detail from "../src/components/detail/Detail"
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Dogs</h1>
       <Switch>
-        <Route path='/home'><Home /></Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/detail/:id">
+          <Detail />
+        </Route>
       </Switch>
     </div>
   );
