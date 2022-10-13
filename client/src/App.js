c28f157676f875b5af1a2dc824a3203ec0b373dd
Javier <wemakesoftx@gmail.com>
2022-10-13 11:20:39 -0600

    feat: add route to /home

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from '../src/components/home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Dogs</h1>
+      <Switch>
+        <Route path='/home'><Home /></Route>
+      </Switch>
     </div>
   );
 }
