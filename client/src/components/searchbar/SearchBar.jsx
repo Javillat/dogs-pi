4dfe45d95fbb3c149ae6dd301158f9f102e3e57e
Javier <wemakesoftx@gmail.com>
2022-10-14 08:40:43 -0600

    feat: add searchbar component

---
+++ client/src/components/searchbar/SearchBar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { getBreedsByName } from '../../redux/actions/Actions';
+//import './SeachBar.css';
+
+export default function SearchBar(){
+    const dispatch = useDispatch();
+    const [breedvalue, setBreedvalue ] = useState("");
+
+    function handleSubmit(event){
+        event.preventDefault();
+        dispatch(getBreedsByName(breedvalue));
+        setBreedvalue('');
+    }
+
+    return (
+        <div className='search_container'>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    className="search_input" 
+                    type="text"
+                    placeholder="Breed..."
+                    value={breedvalue}
+                    onChange={eventclick => setBreedvalue(eventclick.target.value)}
+                    />
+                <input 
+                    type="submit" 
+                    value="Search Breed"
+                    />
+            </form>
+        </div>
+    );
+};
+
