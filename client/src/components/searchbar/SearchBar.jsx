ed5e1baa7a59490c5761acc78397fd2e0a35a091
Javier Villatoro <wemakesoftx@gmail.com>
2022-10-15 10:39:16 -0600

    chore: just format

--- client/src/components/searchbar/SearchBar.jsx
+++ client/src/components/searchbar/SearchBar.jsx
@@ -1,34 +1,30 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { getBreedsByName } from '../../redux/actions/Actions';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getBreedsByName } from "../../redux/actions/Actions";
 //import './SeachBar.css';
 
-export default function SearchBar(){
-    const dispatch = useDispatch();
-    const [breedvalue, setBreedvalue ] = useState("");
+export default function SearchBar() {
+  const dispatch = useDispatch();
+  const [breedvalue, setBreedvalue] = useState("");
 
-    function handleSubmit(event){
-        event.preventDefault();
-        dispatch(getBreedsByName(breedvalue));
-        setBreedvalue('');
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    dispatch(getBreedsByName(breedvalue));
+    setBreedvalue("");
+  }
 
-    return (
-        <div className='search_container'>
-            <form onSubmit={handleSubmit}>
-                <input 
-                    className="search_input" 
-                    type="text"
-                    placeholder="Breed..."
-                    value={breedvalue}
-                    onChange={eventclick => setBreedvalue(eventclick.target.value)}
-                    />
-                <input 
-                    type="submit" 
-                    value="Search Breed"
-                    />
-            </form>
-        </div>
-    );
-};
-
+  return (
+    <div className="search_container">
+      <form onSubmit={handleSubmit}>
+        <input
+          className="search_input"
+          type="text"
+          placeholder="Breed..."
+          value={breedvalue}
+          onChange={(eventclick) => setBreedvalue(eventclick.target.value)}
+        />
+        <input type="submit" value="Search Breed" />
+      </form>
+    </div>
+  );
+}
