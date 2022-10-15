9fb2cdec35b3ff7b8d706d13f4a32b2063e5a212
Javier Villatoro <wemakesoftx@gmail.com>
2022-10-15 10:40:41 -0600

    feat: add Nav component

---
+++ client/src/components/nav/Nav.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SearchBar from "../searchbar/SearchBar";
+
+export default function Nav() {
+  return (
+    <nav className="nav">
+        <SearchBar />
+    </nav>
+  )
+}
