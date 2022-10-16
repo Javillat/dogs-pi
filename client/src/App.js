62802d4942da3afcbf25539378cba617462bacc9
Javier Villatoro <wemakesoftx@gmail.com>
2022-10-16 16:10:36 -0600

    refactor: erase Nav component call

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "../src/components/home/Home";
 import Detail from "../src/components/detail/Detail"
-import Nav from '../src/components/nav/Nav'
+//import Nav from '../src/components/nav/Nav'
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Dogs</h1>
-      <Nav />
-      <Switch>
+       <Switch>
         <Route path="/home">
           <Home />
         </Route>
