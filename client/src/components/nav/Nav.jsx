28194377f699dad3e31862edb545a73e665b8d31
Javier Villatoro <wemakesoftx@gmail.com>
2022-10-17 14:54:58 -0600

    chore: change props and render Order

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SearchBar from "../searchbar/SearchBar";
+//import Order from "../filters/Order";
 
 export default function Nav({setCurrentPage}) {
   return (
     <nav className="nav">
-        <SearchBar setCurrentPage={setCurrentPage}/>
+        <SearchBar setCurrentPage={setCurrentPage} />
+        {/* <Order setCurrentPage={setCurrentPage} /> */}
     </nav>
   )
 }
