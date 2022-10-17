42cb5f5f9e0b9440ae497f8edc12f891aeb65747
Javier Villatoro <wemakesoftx@gmail.com>
2022-10-17 15:08:35 -0600

    feat: auxiliar component

---
+++ client/src/components/filters/Order.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { orderByNameAction } from "../../redux/actions/Actions";
+
+export default function Order({ setCurrentPage, orderByName }) {
+//   const dispatch = useDispatch();
+//   const [orden, setOrden] = useState('');
+
+//   function orderByName(event) {
+//     event.preventDefault();
+//     //setCurrentPage(2);
+//     dispatch(orderByNameAction(event.target.value));
+//     setCurrentPage(1);
+//     setOrden(event.target.value);
+//   }
+
+  return (
+    <section className="order_container">
+      <select
+        className="select_order_name"
+        onChange={(event) => orderByName(event)}
+      >
+        <option value="" defaultValue="">
+          Sort by Name
+        </option>
+        <option value="ASCENDENT">Ascendent Order</option>
+        <option value="DESCENDENT">Descendent Order</option>
+      </select>
+    </section>
+  );
+}
