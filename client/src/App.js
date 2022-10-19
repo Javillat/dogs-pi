45c603db404d1c3218eae60d1f3dcd75181e6463
Javier Villatoro <wemakesoftx@gmail.com>
2022-10-19 11:15:32 -0600

    chore: add path to landing page

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,16 @@ import Home from "../src/components/home/Home";
 import Detail from "../src/components/detail/Detail"
 //import Nav from '../src/components/nav/Nav'
 import "./App.css";
+import LandingPage from "./components/landing/Landing";
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Dogs</h1>
        <Switch>
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
         <Route path="/home">
           <Home />
         </Route>
