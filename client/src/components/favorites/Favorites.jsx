e601aa12907b90e8d555a8f3b29efe3e006aa48a
Javier Villatoro <wemakesoftx@gmail.com>
2022-10-24 17:47:26 -0600

    feat: add favorites functional component and css

---
+++ client/src/components/favorites/Favorites.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { removeFavorites } from "../../redux/actions/Actions";
+import "./Favorites.css";
+
+export default function Favorites() {
+  //console.log(propsHome);
+  const favorites = useSelector((data) => data.favorites);
+  const dispatch = useDispatch();
+
+  const handleClick = (id) => {
+    dispatch(removeFavorites(id));
+  };
+  return (
+    <>
+      <div>
+        <Link to="/home">
+          <button className="button load_all_home">Go Home</button>
+        </Link>
+      </div>
+      <div className="container">
+        {favorites.map((favorites) => {
+          return (
+            <div className="card_favorites">
+              <div className="image">
+                <Link to={`/detail/${favorites.id}`}>
+                  <img src={favorites.image} alt="Breed" />
+                </Link>
+              </div>
+              <div className="content">
+                <h3 className="name_favorites">{favorites.name}</h3>
+                <div className="weight">
+                  {/* <h3>Weight</h3> */}
+                  <h4>
+                    {/* Min {propsHome.min_weight} Max {propsHome.max_weight} */}
+                  </h4>
+                </div>
+                {/* <h4 className="temperaments">{propsHome.temperament}</h4> */}
+                <button
+                  className="remove_favorites"
+                  onClick={() => handleClick(favorites.id)}
+                >
+                  REMOVE
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
