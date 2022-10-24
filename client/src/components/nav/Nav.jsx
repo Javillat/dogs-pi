4ac92e708617abeda00a7ae331d88932459f6208
Javier Villatoro <wemakesoftx@gmail.com>
2022-10-23 18:37:22 -0600

    feat: add create breed button

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import SearchBar from "../searchbar/SearchBar";
 //import Order from "../filters/Order";
 
-export default function Nav({setCurrentPage}) {
+export default function Nav({ setCurrentPage }) {
   return (
     <nav className="nav">
-        <SearchBar setCurrentPage={setCurrentPage} />
-        {/* <Order setCurrentPage={setCurrentPage} /> */}
+      <SearchBar setCurrentPage={setCurrentPage} />
+      <div className="create_button">
+        <Link to="/create">
+          <button>Create Breed</button>
+        </Link>
+      </div>
+      {/* <Order setCurrentPage={setCurrentPage} /> */}
     </nav>
-  )
+  );
 }
