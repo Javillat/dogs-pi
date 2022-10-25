fbea8ed08cb144c6cd4de3b5e36a9318edf27d43
Javier Villatoro <wemakesoftx@gmail.com>
2022-10-24 19:56:52 -0600

    chore: change component button add css

--- client/src/components/landing/Landing.jsx
+++ client/src/components/landing/Landing.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import dog from "../../img/dog.png";
+import "./Landing.css"
 
 const LandingPage = () => {
     return(
         <div>
             <Link to="/home">
-                <button className="home_button">Watch dog breeds</button>
+                <button className="button1 home_button">Watch dog breeds</button>
             </Link>
             <img src={dog} alt="Loading" />
         </div>
