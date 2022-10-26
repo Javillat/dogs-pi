e6a7ec8b1cc0b4bb1c5f00b3295ccf54a07afdab
Javier Villatoro <wemakesoftx@gmail.com>
2022-10-26 14:29:33 -0600

    feat: clean state

--- client/src/components/detail/Detail.jsx
+++ client/src/components/detail/Detail.jsx
@@ -15,10 +15,13 @@ export default function Detail() {
   const [isLoading, setLoading] = useState(false);
   useEffect(() => {
     setLoading(true);
-    dispatch(getDetail(id)); //LIMPIAR EL STATE
-    // return () => {
-    //   dispatch(getClean());
-    // };
+    let disp = dispatch(getDetail(id)); //LIMPIAR EL STATE
+    console.log('disp', disp);
+    //dispatch(getClean())
+    setLoading(false);
+    return () => {
+        dispatch(getClean());
+    }
   }, []);
 
   // useEffect(() => {
@@ -29,63 +32,73 @@ export default function Detail() {
     history.push("/home");
   };
 
-  return detail !== undefined ? (
+  //   return detail !== undefined ? (
+  return (
     <>
-      <span>
-        <button className="buttond button_detail" onClick={backHandler}>Go Back</button>
-      </span>
-      <div className="detail_container">
-        <div className="image_container">
-          <img src={detail.image} alt="Breed" />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div>
+          <span>
+            <button className="buttond button_detail" onClick={backHandler}>
+              Go Back
+            </button>
+          </span>
+          <div className="detail_container">
+            <div className="image_container">
+              <img src={detail.image} alt="Breed" />
+            </div>
+            <div className="content_details">
+              <div className="id_detail">
+                <h3 className="white">ID:</h3>
+                <h4>{detail.id}</h4>
+              </div>
+              <div className="name_detail">
+                <h3 className="white">
+                  Name<h4>{detail.name}</h4>
+                </h3>
+              </div>
+              <div className="height_detail">
+                <h3 className="white">
+                  Min Height <h4>{detail.min_height}</h4>
+                </h3>
+                <h3 className="white">
+                  Max Height <h4>{detail.max_height}</h4>
+                </h3>
+              </div>
+              <div className="weight_detail">
+                <h3 className="white">
+                  Min Weight <h4>{detail.min_weight}</h4>
+                </h3>
+                <h3 className="white">
+                  Max Weight <h4>{detail.max_weight}</h4>
+                </h3>
+              </div>
+              <div className="origin_detail">
+                <h3 className="white">
+                  Origin<h4>{detail.origin}</h4>
+                </h3>
+              </div>
+              <div className="lifespan_detail">
+                <h3 className="white">
+                  Min Lifespan<h4> {detail.min_life_span}</h4>
+                </h3>
+                <h3 className="white">
+                  Max Lifespan<h4>{detail.max_life_span}</h4>
+                </h3>
+              </div>
+              <div className="temperaments_detail">
+                <h3 className="white">
+                  Temperaments<h4>{detail.temperament}</h4>
+                </h3>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="content_details">
-          <div className="id_detail">
-            <h3>ID:</h3>
-            <h4>{detail.id}</h4>
-          </div>
-          <div className="name_detail">
-            <h3>
-              Name<h4>{detail.name}</h4>
-            </h3>
-          </div>
-          <div className="height_detail">
-            <h3>
-              Min Height <h4>{detail.min_height}</h4>
-            </h3>
-            <h3>
-              Max Height <h4>{detail.max_height}</h4>
-            </h3>
-          </div>
-          <div className="weight_detail">
-            <h3>
-              Min Weight <h4>{detail.min_weight}</h4>
-            </h3>
-            <h3>
-              Max Weight <h4>{detail.max_weight}</h4>
-            </h3>
-          </div>
-          <div className="origin_detail">
-            <h3>
-              Origin<h4>{detail.origin}</h4>
-            </h3>
-          </div>
-          <div className="lifespan_detail">
-            <h3>
-              Min Lifespan<h4> {detail.min_life_span}</h4>
-            </h3>
-            <h3>
-              Max Lifespan<h4>{detail.max_life_span}</h4>
-            </h3>
-          </div>
-          <div className="temperaments_detail">
-            <h3>
-              Temperaments<h4>{detail.temperament}</h4>
-            </h3>
-          </div>
-        </div>
-      </div>
+      )}
     </>
-  ) : (
-    <Loading />
   );
+  //   ) : (
+  //     <Loading />
+  //   );
 }
