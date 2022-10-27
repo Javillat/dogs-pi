bcca5d0c55bf8eb2d3816456d8172438d490126a
Javier Villatoro <wemakesoftx@gmail.com>
2022-10-27 09:14:14 -0600

    feat: add test back, model dog and dog route

--- api/tests/models/dog.spec.js
+++ api/tests/models/dog.spec.js
@@ -8,15 +8,72 @@ describe('Dog model', () => {
     }));
   describe('Validators', () => {
     beforeEach(() => Dog.sync({ force: true }));
-    describe('name', () => {
-      it('should throw an error if name is null', (done) => {
+
+    describe('id', () =>{
+      it('Debe mostrar un error si id es null', (done)=> {
         Dog.create({})
-          .then(() => done(new Error('It requires a valid name')))
+        .then(()=> done(new Error('Requiere que id no sea null')))
+        .catch(() => done());
+      });
+      it('Debe pasar sin problemas si tiene un id valido', ()=>{
+        Dog.create({ id:"BD-10"});
+      })
+    });
+
+    describe('name', () => {
+      it('Debe mostrar un error si name es null', (done) => {
+        Dog.create({})
+          .then(() => done(new Error('Requiere un name valido')))
           .catch(() => done());
       });
-      it('should work when its a valid name', () => {
+      it('Debe pasar sin problema si tiene un name valido', () => {
         Dog.create({ name: 'Pug' });
       });
     });
+
+    describe('minheight', () =>{
+      it('Debe mostrar un error si minheight es null', (done)=> {
+        Dog.create({})
+        .then(()=> done(new Error('Requiere que minheight no sea null')))
+        .catch(() => done());
+      });
+      it('Debe pasar sin problemas si tiene un minheight valido', ()=>{
+        Dog.create({ minheight:2});
+      })
+    });
+
+    describe('maxheight', () =>{
+      it('Debe mostrar un error si maxheight es null', (done)=> {
+        Dog.create({})
+        .then(()=> done(new Error('Requiere que maxheight no sea null')))
+        .catch(() => done());
+      });
+      it('Debe pasar sin problemas si tiene un maxheight valido', ()=>{
+        Dog.create({ maxheight:5});
+      })
+    });
+
+    describe('minweight', () =>{
+      it('Debe mostrar un error si minweight es null', (done)=> {
+        Dog.create({})
+        .then(()=> done(new Error('Requiere que minweight no sea null')))
+        .catch(() => done());
+      });
+      it('Debe pasar sin problemas si tiene un minweight valido', ()=>{
+        Dog.create({ minweight:4});
+      })
+    });
+
+    describe('maxweight', () =>{
+      it('Debe mostrar un error si maxweight es null', (done)=> {
+        Dog.create({})
+        .then(()=> done(new Error('Requiere que maxweight no sea null')))
+        .catch(() => done());
+      });
+      it('Debe pasar sin problemas si tiene un maxweight valido', ()=>{
+        Dog.create({ maxweight:56});
+      })
+    });
+    
   });
 });
--- api/tests/routes/dog.spec.js
+++ api/tests/routes/dog.spec.js
@@ -1,24 +1,33 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { expect } = require('chai');
-const session = require('supertest-session');
-const app = require('../../src/app.js');
-const { Dog, conn } = require('../../src/db.js');
+const { expect } = require("chai");
+const session = require("supertest-session");
+const app = require("../../src/app.js");
+const { Dog, conn } = require("../../src/db.js");
 
 const agent = session(app);
 const dog = {
-  name: 'Pug',
+  name: "Pug",
 };
 
-describe('Videogame routes', () => {
-  before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
-  beforeEach(() => Dog.sync({ force: true })
-    .then(() => Dog.create(dog)));
-  describe('GET /dogs', () => {
-    it('should get 200', () =>
-      agent.get('/dogs').expect(200)
-    );
+describe("Dogs routes", () => {
+  before(() =>
+    conn.authenticate().catch((err) => {
+      console.error("Unable to connect to the database:", err);
+    })
+  );
+  beforeEach(() =>
+    Dog.sync({ force: true }).then(() =>
+      Dog.create({
+        id:"BD-12",
+        name: "Brida",
+        minheight: 2,
+        maxheight: 5,
+        minweight: 4,
+        maxweight: 7,
+      })
+    )
+  );
+  describe("GET /dogs", () => {
+    it("should get 200", () => agent.get("/dogs").expect(200));
   });
 });
