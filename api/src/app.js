90ed29eb1cbee626fbf6aac68aacf48656976b10
Javier Villatoro <wemakesoftx@gmail.com>
2022-12-18 19:52:26 -0600

    fix: update and fix CORS array multiple origin

--- api/src/app.js
+++ api/src/app.js
@@ -15,12 +15,17 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
+  const allowedOrigins = ['http://localhost:3000', 'https://javier-dogs.vercel.app'];
+  const origin = req.headers.origin;
+  if(allowedOrigins.includes(origin)){
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
   //res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Origin', 'https://javier-dogs.vercel.app' || 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  //res.header('Access-Control-Allow-Origin', 'https://javier-dogs.vercel.app' || 'http://localhost:3000'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
 });
 
 server.use('/', routes);
