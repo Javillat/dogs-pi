872e2947f9b7fbc8085a200e6966f430fc2171a5
Javier Villatoro <wemakesoftx@gmail.com>
2024-11-07 10:04:44 -0600

    fix Cors sin respuesta

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-
+const cors = require('cors');
 require('./db.js');
 
 const server = express();
@@ -14,26 +14,41 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-  const allowedOrigins = [
-    'http://localhost:3000', 
-    'https://javier-dogs.vercel.app', 
-    'dogs-1qswf01wt-javier-villatotos-projects.vercel.app', 
-    'dogs-git-main-javier-villatotos-projects.vercel.app'
-  ];
-  const origin = req.headers.origin;
-  console.log('Presente header',origin);
-  
-  if(allowedOrigins.includes(origin)){
-    res.setHeader('Access-Control-Allow-Origin', origin);
+server.use(cors(
+  {
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://javier-dogs.vercel.app',
+      'dogs-1qswf01wt-javier-villatotos-projects.vercel.app',
+      'dogs-git-main-javier-villatotos-projects.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Origin', 'Content-Type', 'Authorization', 'Authorization', 'Accept'],
+    credentials: true,
   }
-  //res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  //res.header('Access-Control-Allow-Origin', 'https://javier-dogs.vercel.app' || 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept');
-  res.header('Access-Control-Allow-Credentials', true);
-  return next();
-});
+));
+
+// server.use(cors({
+//   const allowedOrigins = [
+//     'http://localhost:3000', 
+//     'https://javier-dogs.vercel.app', 
+//     'dogs-1qswf01wt-javier-villatotos-projects.vercel.app', 
+//     'dogs-git-main-javier-villatotos-projects.vercel.app'
+//   ];
+//   const origin = req.headers.origin;
+//   console.log('Presente header',origin);
+
+//   if(allowedOrigins.includes(origin)){
+//     res.setHeader('Access-Control-Allow-Origin', origin);
+//   }
+//   //res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+//   //res.header('Access-Control-Allow-Origin', 'https://javier-dogs.vercel.app' || 'http://localhost:3000'); // update to match the domain you will make the request from
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept');
+//   res.header('Access-Control-Allow-Credentials', true);
+//   return next();
+// });
+// )
 
 server.use('/', routes);
 
